9631d099182b680e08fee8e32fe3c293132b809e
Thar Htet San <tharhtet1234@gmail.com>
2024-09-08 10:58:56 +0700

    add init
    getting start with fastapi

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    version = os.getenv("APP_VERSION")
+    dev_env = os.getenv("ENV")
+    str_out = "Hello world this is {} -  server version : {}".format(dev_env,version)
+    return str_out
+
+
+if __name__== "__main__":
+    uvicorn.run("main:app",host='0.0.0.0',port=8080,reload=True)
